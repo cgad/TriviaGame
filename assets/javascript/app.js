9fe25be9770d3a95b2490713693da0dbf8513b7b
cgad <35946778+cgad@users.noreply.github.com>
2018-08-13 22:58:04 -0600

    wronganswer bug fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,6 @@ var incorrect = 0;
 var unanswered = 0;
 var timeLeft = 20;
 var intervalId;
-// var timerOn = false;
-// var timerId;
 var qArrayPos = 0;
 var playerAnswer;
 
@@ -31,36 +29,29 @@ var questions = [
 // FUNCTIONS
 
 function timer() {
-    // timeLeft = 20;
     clearInterval(intervalId);
-    // timerOn = false;
     intervalId = setInterval(decrement, 1000);
 }
 
 function stop() {
     clearInterval(intervalId);
-    // timerOn = true;
 }
 
 function decrement() {
-    // clearInterval(timerId);
-    // timerId = setInterval(decrement, 1000);
-    // if (!timerOn) {
-        timeLeft--;
-        $("#timer").html("<p>Time remaining: " + timeLeft + " seconds");
-        if (timeLeft === 0) {
-            stop();
-            $("#timer").html("<strong>Time's up!</strong>");
-            $("#show-answer").html("The correct answer is: " + questions[qArrayPos].c);
-            // reset timeleft each time it = 0
-            timeLeft = 20;
-            setTimeout(contentPrint, 3000);
-            qArrayPos++;
-            unanswered++;
-            $("#no-answer").html("Unanswered: " + unanswered);
-            $("#answers").html(""); 
-        }
-    // }
+    timeLeft--;
+    $("#timer").html("<p>Time remaining: " + timeLeft + " seconds");
+    if (timeLeft === 0) {
+        stop();
+        $("#timer").html("<strong>Time's up!</strong>");
+        $("#show-answer").html("The correct answer is: " + questions[qArrayPos].c);
+        // reset timeleft each time it = 0
+        timeLeft = 20;
+        setTimeout(contentPrint, 3000);
+        qArrayPos++;
+        unanswered++;
+        $("#no-answer").html("Unanswered: " + unanswered);
+        $("#answers").html(""); 
+    }
 }
 
 function contentPrint() {
@@ -78,7 +69,7 @@ function contentPrint() {
         $("#question").html("<h4>" + questions[qArrayPos].q + "</h4>");
         $("#answers").html("");
         for (var i = 0; i < questions[qArrayPos].a.length; i++) {
-            $("#answers").append("<p id='answer" + i + "' class='ans'><strong>" + questions[qArrayPos].a[i] + "</strong></p>");
+            $("#answers").append("<p id='answer" + i + "' class='ans'>" + questions[qArrayPos].a[i] + "</p>");
         }
     } else if (correct + incorrect + unanswered === questions.length) {
         endGame();
@@ -93,8 +84,6 @@ function rightAnswer() {
     $("#answers").html("");
     $("#timer").html("");
     qArrayPos++;
-    // stop();
-    // reset timeleft each time it = 0
     timeLeft = 20;
     setTimeout(contentPrint, 3000);
 }
@@ -107,7 +96,6 @@ function wrongAnswer() {
     $("#answers").html("");
     $("#timer").html("");
     qArrayPos++;
-    // stop();
     // reset timeleft each time it = 0
     timeLeft = 20;
     setTimeout(contentPrint, 3000);
@@ -120,9 +108,6 @@ function endGame() {
     $("#question").html("");
     $("#answers").html("");
     $("#previous").html("<p></p><h5>Your previous score was</h5>");
-    // $("#right").html("");
-    // $("#wrong").html("");
-    // $("#no-answer").html("");
     $("#restart").html("<p></p><button type='button' class='btn btn-success start'>Restart Game</button>");
     stop();
     qArrayPos = 0;
@@ -133,11 +118,13 @@ function endGame() {
 $(".start").on("click", function() {
     $(".start").remove();
     contentPrint();
+    console.log(questions[qArrayPos].c);
 })
 
-$("#answers").on("click", ".ans", function() { // .ans is child of the div // event delegation
+$("#answers").on("click", ".ans", function() {
     var playerAnswer = $(this).html();
     stop();
+    console.log(playerAnswer);
     if (playerAnswer === questions[qArrayPos].c) {
         rightAnswer();
     } else {
